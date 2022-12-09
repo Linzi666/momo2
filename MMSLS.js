8d74babebf73ced785d1073d2f21e37a020eaa13
Linzi666 <109735520+Linzi666@users.noreply.github.com>
2022-12-09 20:58:31 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -22,10 +22,10 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=24142114&pid=21b2401b3d80aff3486ba63354142110&tid=800aac44c9db90d33566e78a02fbe45e",
-    "https://www.maimemo.com/share/page?uid=24142114&pid=21b2401b3d80aff3486ba63354142110&tid=800aac44c9db90d33566e78a02fbe45e",
-    "https://www.maimemo.com/share/page?uid=24142114&pid=21b2401b3d80aff3486ba63354142110&tid=800aac44c9db90d33566e78a02fbe45e",
-    "https://www.maimemo.com/share/page?uid=24142114&pid=21b2401b3d80aff3486ba63354142110&tid=800aac44c9db90d33566e78a02fbe45e",
+    "https://www.maimemo.com/share/page?uid=24142114&pid=122dee1b3261092d4037ab54b65b8028&tid=14c9bd5b6bff907b7421d740a3c99601",
+    "https://www.maimemo.com/share/page?uid=24142114&pid=122dee1b3261092d4037ab54b65b8028&tid=14c9bd5b6bff907b7421d740a3c99601",
+    "https://www.maimemo.com/share/page?uid=24142114&pid=122dee1b3261092d4037ab54b65b8028&tid=14c9bd5b6bff907b7421d740a3c99601",
+    "https://www.maimemo.com/share/page?uid=24142114&pid=122dee1b3261092d4037ab54b65b8028&tid=14c9bd5b6bff907b7421d740a3c99601",
   ]
   /**
    * 生成随机数字
